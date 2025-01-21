3740cd250adf93d51bd243a04f2950d59e28c8c9
rudy282 <121287489+rudy282@users.noreply.github.com>
2025-01-21 10:50:47 +0100

    initialized supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createClient, PostgrestSingleResponse } from "@supabase/supabase-js";
+import "./App.css";
+import { useEffect, useState } from "react";
+import { iQuestion } from "./interfaces";
+
+const key = import.meta.env.VITE_KEY || "";
+const url = import.meta.env.VITE_URL || "";
+
+const supabase = createClient(url, key);
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [questions, setQuestions] = useState<iQuestion[]>([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        getQuestions();
+    });
+
+    async function getQuestions() {
+        const response: PostgrestSingleResponse<iQuestion[]> = await supabase
+            .from("questions")
+            .select();
+        setQuestions(response.data || []);
+    }
+
+    return (
+        <>
+            <ul>
+                {questions.map((question) => (
+                    <li key={question.id}>{question.question}</li>
+                ))}
+            </ul>
+        </>
+    );
 }
 
-export default App
+export default App;
---
+++ src/interfaces.ts
@@ -0,0 +1,9 @@
+export interface iQuestion {
+    id: string;
+    question: string;
+    correct_ans: string;
+    ans1: string;
+    ans2: string;
+    ans3: string;
+    category: string;
+}
